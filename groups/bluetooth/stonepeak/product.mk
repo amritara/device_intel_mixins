047d16ad4afefef3c22b48d7bd9bf523a2d4f94f
Petre Pircalabu <petre.ovidiu.pircalabu@intel.com>
2015-06-10 15:46:51 +0300

    bluetooth: Add support for Intel 7260AC (StonePeak2)
    Change-Id: I34b46d6e80461297bdf1167af64fcbfa97f84197
    Depends-Change-Id: Ife72df7cf68e25d2310517208dd6cfcf9081ac94
    Tracked-On: https://jira01.devtools.intel.com/browse/A01-60
    Signed-off-by: Petre Pircalabu <petre.ovidiu.pircalabu@intel.com>
    Reviewed-on: https://android.intel.com/379146
    Reviewed-by: Serban, Mihai <mihai.serban@intel.com>

--- groups/bluetooth/stonepeak/product.mk
+++ groups/bluetooth/stonepeak/product.mk
@@ -6,3 +6,4 @@ PRODUCT_COPY_FILES += frameworks/native/data/etc/android.hardware.bluetooth.xml:
 
 PRODUCT_COPY_FILES += \
         vendor/intel/fw/bluetooth/intel/ibt-hw-37.8.10-fw-1.10.2.27.d.bseq:system/etc/firmware/intel/ibt-hw-37.8.10-fw-1.10.2.27.d.bseq:intel \
+        vendor/intel/fw/bluetooth/intel/ibt-hw-37.7.10-fw-1.80.2.3.d.bseq:system/etc/firmware/intel/ibt-hw-37.7.10-fw-1.80.2.3.d.bseq:intel \
