0f20fa5dcd933165bce0be342b169a17828fef78
Stefan Stanacar <stefan.stanacar@intel.com>
2015-09-28 13:28:21 +0300

    Remove reference to development adb keys
    These don't make sense unless you have the test keys which
    are used usually for test machines.
    
    Change-Id: Ia9c324ede30e54b6be04323a75ad997556a6f9f3
    Signed-off-by: Stefan Stanacar <stefan.stanacar@intel.com>

--- groups/usb-gadget/g_android/product.mk
+++ groups/usb-gadget/g_android/product.mk
@@ -1,18 +1,6 @@
-# Set default USB interface
-ifeq ($(BUILD_FOR_CTS_AUTOMATION),true)
-# Build for automated CTS
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += persist.sys.usb.config=mtp,adb
-PRODUCT_COPY_FILES += device/intel/common/usb-gadget/adb_keys:root/adb_keys
-else
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += persist.sys.usb.config=mtp
-endif
 
 # Enable Secure Debugging
 ifeq ($(TARGET_BUILD_VARIANT),user)
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.adb.secure=1
-endif
-
-# Add Intel adb keys for userdebug/eng builds
-ifneq ($(TARGET_BUILD_VARIANT),user)
-PRODUCT_COPY_FILES += device/intel/common/usb-gadget/adb_keys:root/adb_keys
 endif
